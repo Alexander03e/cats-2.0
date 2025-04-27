5f82e9ae45d948f946ee4c03ebb460a6f55af333
Namisami <79804458+Namisami@users.noreply.github.com>
2025-04-27 21:52:38 +0300

    add auth

---
+++ backend/authentication/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+from .views import (
+    UserViewSet,
+    AuthViewSet
+)
+
+router = SimpleRouter()
+
+router.register(r'user', UserViewSet, basename='user')
+router.register(r'', AuthViewSet, basename='auth')
