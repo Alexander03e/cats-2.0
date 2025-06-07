2926e11ba9ccc9ae8659b0e716508bcbbffe895e
cfifeg1@gmail.com <cfifeg1@gmail.com>
2025-06-07 21:09:43 +0300

    add system info admin

--- backend/system/admin.py
+++ backend/system/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import System
 
-# Register your models here.
+
+@admin.register(System)
+class SystemAdmin(admin.ModelAdmin):
+    list_display = ('address', 'phone_number', 'email')
+    search_fields = ('address', 'phone_number', 'email')
+    list_filter = ('vk_link', 'telegram_link', 'instagram_link', 'whatsapp_link')
+    fieldsets = (
+        (None, {
+            'fields': ('address', 'phone_number', 'email')
+        }),
+        ('Социальные сети', {
+            'fields': ('vk_link', 'telegram_link', 'instagram_link', 'whatsapp_link'),
+            'classes': ('wide',)
+        }),
+        ('Рабочее время', {
+            'fields': ('calendar_info', 'short_calendar_info'),
+        }),
+    )
