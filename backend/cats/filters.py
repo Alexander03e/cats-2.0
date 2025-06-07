c2de24d32679a9efaabdde88918e34a75bd17094
cfifeg1@gmail.com <cfifeg1@gmail.com>
2025-06-08 01:07:45 +0300

    fix color filter

--- backend/cats/filters.py
+++ backend/cats/filters.py
@@ -1,14 +1,22 @@
 import django_filters
+from django.db.models import Q
 from .models import Cat
+
 
 class CatFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
     gender = django_filters.MultipleChoiceFilter(choices=Cat.GENDER_CHOICES)
-    color = django_filters.MultipleChoiceFilter()
+    color = django_filters.CharFilter(method='filter_by_colors')  # Custom filter method
     health_status = django_filters.MultipleChoiceFilter(choices=Cat.HEALTH_STATUS_CHOICES)
     coat_type = django_filters.MultipleChoiceFilter(choices=Cat.COAT_TYPE_CHOICES)
 
     class Meta:
         model = Cat
         fields = []
-        
+
+    def filter_by_colors(self, queryset, name, value):
+        colors = value.split(',')
+        query = Q()
+        for color in colors:
+            query |= Q(color__icontains=color.strip())
+        return queryset.filter(query)
