a6560bc67cb002a4029d0a89ebfdec92121df4aa
cfifeg1@gmail.com <cfifeg1@gmail.com>
2025-06-08 23:02:27 +0300

    fix

--- frontend/src/shared/api/index.ts
+++ frontend/src/shared/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BACKEND_URL = 'http://109.73.194.62:8000/';
+export const BACKEND_URL = 'https://api.котодом-самара.рф/';
 
 export const $api = axios.create({
     baseURL: BACKEND_URL,
