a77155a27e07e39f714546fc5a82ddde13b00ab2
cfifeg1@gmail.com <cfifeg1@gmail.com>
2025-06-09 16:25:49 +0300

    fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -81,7 +81,7 @@ REST_FRAMEWORK = {
 }
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
