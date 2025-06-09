caef7f73f2b89dc7994cc5927ecd138b7e34fdac
cfifeg1@gmail.com <cfifeg1@gmail.com>
2025-06-09 16:25:37 +0300

    fix

--- admin/src/shared/utils/createFileFromUrl.ts
+++ admin/src/shared/utils/createFileFromUrl.ts
@@ -4,6 +4,9 @@ export const createFileFromUrl = async (url: string, fileName: string) => {
     console.log(secureUrl);
 
     const response = await fetch(secureUrl);
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
     const blob = await response.blob();
     return new File([blob], fileName, { type: blob.type });
 };
